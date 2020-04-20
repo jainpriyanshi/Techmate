6782283af408b09483fa94667a70a84a2784dc9a
jparul26 <jain.26@iitj.ac.in>
2020-04-20 16:18:39 +0530

    profile updated

--- client/src/components/Profilepage/profile.jsx
+++ client/src/components/Profilepage/profile.jsx
@@ -12,7 +12,14 @@ import P3 from'../Images/p3.jpg';
 import P4 from'../Images/cup.png';
 import college from'../Images/college.png';
 import L from '../Images/contact.png';
+import MailIcon from '@material-ui/icons/Mail';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import CUP from '../Images/skills.jpg';
+import './profile.css'
+import GitHubIcon from '@material-ui/icons/GitHub';
+import Code from '@material-ui/icons/Person';
+import C from '@material-ui/icons/CheckBox';
+
 
 
 
@@ -47,135 +54,168 @@ class profilebyid extends Component {
                 );
             if(users._id===this.props.match.params.id)
             return (
-                <div>    
-               
                <div>
-                     <p style={{color:"grey",fontSize:"100px",marginTop:"50px",textAlign:"center"}}>Profile</p>
-                   </div>
-                 <div>
-                    <div style={{float:"left"}}>
-                         <img src={P1} style={{height:"450px" ,width:"450px",marginTop:"50px",marginLeft:"150px"}}/> 
-                    </div>
-                    <div style={{float:"Right",textAlign:"center",marginRight:"90px"}}>
-                    <h4 style={{marginTop:"250px",color:"grey"}}>Hey There!</h4>
-                    <h1 style={{marginTop:"10px",fontSize:"75px"}}><b>I am {users.name}</b> </h1>
-                    </div>
+                  <div id="user-profile-2" class="user-profile" style={{marginTop:"10%"}}>
+                  <div class="tabbable">
+                  <ul class="nav nav-tabs padding-18">
+				<li class="active">
+					<a data-toggle="tab" href="#home">
+						<i class="green ace-icon fa fa-user bigger-120"></i>
+						Profile
+					</a>
+				</li>
+			</ul>
+            <div class="tab-content no-border padding-24">
+				<div id="home" class="tab-pane in active">
+					<div class="row">
+						<div class="col-xs-12 col-sm-3 center">
+                        <span class="profile-picture">
+							                    	<img class="editable img-responsive" alt=" Avatar" id="avatar2" src={P1} style={{width:"100%",height:"100%"}} ></img>  
 
-                </div>
-                <div >
-                     <div style={{float:"Right",marginTop:"50px"}}>
-                         <img src={P2}/>
-                     </div >  
-                     <div  style={{float:"left",width:"450px",marginLeft:"90px",marginTop:"70px"}}>
-                     {!!(users.bio)?<div><h2>Let's Introduce About Myself</h2><p style={{marginTop:"10px",color:"grey"}}>{users.bio} </p></div>:users.bio}
-                     {!!(users.college)?
-                  
-                    <h2 > Institute:  {users.college} </h2> 
-                     :users.college}
-                     {!!(users.degree)?<h4 style={{textAlign:"center",color:"grey",marginLeft:"-100px"}}>{users.degree}</h4>:users.degree }
-                    </div>   
-                </div>
-               <div>
-               <div style={{float:"Left"}}>
-                   <img src={L} style={{height:"300px",width:"600px",marginTop:"50px",marginLeft:"20px"}}/>
+						            </span>
+                            <div class="space space-4"></div>
+                            {!!(users.linkedin)? <div> 
+                            <a href={users.linkedin} class="btn btn-sm btn-block btn-info ">
+                                <LinkedInIcon style={{ color: "lightblue" }}/>
+								                   <span class="bigger-110">Linkedin</span>
+						               	</a>
 
-               </div>
-               <div style={{float:"Right"}}>
-               <h4 style={{marginTop:"10px",marginRight:"100px"}}><b> Email: <a href={users.email} >{users.email}</a></b> </h4> 
-               {!!(users.linkedin)?<h4 style={{marginTop:"10px",marginRight:"100px"}}><b> Linkedin: <a href={users.linkedin} >{users.linkedin}</a></b> </h4>:users.linkedin}
-               {!!(users.github)?<h4 style={{marginTop:"10px",marginRight:"100px"}}><b> GitHub: <a href={users.github} >{users.github}</a></b></h4>:users.linkedin}
-               {!!(users.codechef)?<h4 style={{marginTop:"10px",marginRight:"100px"}}><b> Codechef: <a href={users.codechef} >{users.codechef}</a></b> </h4>:users.codechef}
-               {!!(users.codeforces)?<h4 style={{marginTop:"10px",marginRight:"100px"}}><b> codeforces: <a href={users.codeforces} >{users.codeforces}</a></b> </h4>:users.codeforces}
-               {!!(users.spoj)?<h4 style={{marginTop:"10px",marginRight:"100px"}}><b> Spoj: <a href={users.spoj} >{users.spoj}</a></b> </h4>:users.spoj}
-               </div>
-               </div>
-               <div >
-               <div style={{float:"Left",marginTop:"400px",marginLeft:"-500px"}}>
-                   <Card style={{height:"300px",width:"400px"}}>
-                       <div style={{marginLeft:"20px"}}>
-               <h4 style={{color:"blue",marginBotton:"10px",marginTop:"10px"}}><b>Skills & Endorsement: </b></h4>
-                       <h5 style={{fontFamily:"Roboto",color:"grey",marginLeft:"10px"}}>   {badgeItems}
-                          </h5>
-                          </div>
-                          </Card>
-               </div>
-               <div style={{float:"Left"}}>
-                 <img src={CUP} style={{height:"200px",width:"200px",marginTop:"400px"}}></img>
-               </div>
-               <div style={{float:"Right",marginTop:"400px",marginRight:"-400px"}}>
-               
-                   <Card style={{height:"300px",width:"400px"}}>
-                       <div style={{marginLeft:"20px"}}>
-                      
-               <h4 style={{color:"blue",marginBotton:"10px",marginTop:"10px"}}><b>Achievement:               </b></h4>
-                      {users.achievement.map((achievement,index)=>(
-                      achievement!== null?<h5 style={{fontFamily:"Roboto",color:"grey"}}> {achievement}</h5>
-                      :null
-                     ))}
+                            </div>:users.linkedin}
+                          
+						                  	<a href={users.email} class="btn btn-sm btn-block btn-primary">
+                                       <MailIcon style={{ color: "lightred" }}/>
+								                       <span class="bigger-110">Send an email</span>
+						                       	</a>
+                         </div>
+                         <div class="col-xs-12 col-sm-9">
+                         <h4 class="blue">
+								<span class="middle" style={{marginLeft:"20px"}}>Hey There!</span>
+							</h4>
+                            <div class="profile-user-info">
+								<div class="profile-info-row">
+                                <div class="profile-info-name"> Username: </div>
+                                <div class="profile-info-value">
+                                  <span>{users.name}</span>
+								</div> 
+                                 
+                                </div>
+                                <div class="profile-info-row">
+                                <div class="profile-info-name"> Institute: </div>
+                                <div class="profile-info-value">
+                                  <span>{users.college}</span>
+								</div> 
+                                 
+                                </div>
+                                <div class="profile-info-row">
+                                <div class="profile-info-name"> Degree: </div>
+                                <div class="profile-info-value">
+                                  <span>{users.degree}</span>
+								</div> 
+                                 
+                                </div>
+                                {!!(users.codechef)?  <div class="profile-info-row">
+									<div class="profile-info-name">
+                                      <Code style={{ color: "lightred" }}/>
+									</div>
+
+									<div class="profile-info-value">
+										<a href={users.codechef}>Find me on Codechef</a>
+									</div>
+								</div>:users.codechef}
+                                {!!(users.codeforces)?  <div class="profile-info-row">
+									<div class="profile-info-name">
+                                      <Code style={{ color: "lightred" }}/>
+									</div>
+
+									<div class="profile-info-value">
+										<a href={users.codeforces}>Find me on Codeforces</a>
+									</div>
+								</div>:users.codeforces}
+                                {!!(users.spoj)?  <div class="profile-info-row">
+									<div class="profile-info-name">
+                                      <Code style={{ color: "lightred" }}/>
+									</div>
+
+									<div class="profile-info-value">
+										<a href={users.spoj}>Find me on Spoj</a>
+									</div>
+								</div>:users.spoj}
+                              {!!(users.github)?  <div class="profile-info-row">
+									<div class="profile-info-name">
+                                      <GitHubIcon style={{ color: "lightred" }}/>
+									</div>
+
+									<div class="profile-info-value">
+										<a href={users.github}>Find me on Github</a>
+									</div>
+								</div>:users.github}
+
+                            </div> 
+                              
+                         </div>   
                      </div>
-                     </Card>
-               </div>
-               </div>
-
-      
+                     <div class="space-20"></div>
+                     <div class="row" style={{marginTop:"15px"}}>
+						<div class="col-xs-12 col-sm-6">
+							<div class="widget-box transparent">
+								<div class="widget-header widget-header-small">
+                                <h4 class="widget-title smaller">
+                                <C style={{ color: "black" }}/>
+										Little About Me
+									</h4>
+                                 </div>
+                                 <div class="widget-body">
+									<div class="widget-main">
+                                        {!!(users.bio)?<p>{users.bio}</p>:users.bio}
+                                   </div>
+                                 </div>  
+                                 <div class="row" style={{marginTop:"15px"}}>
+                                 <div class="col-sm-12">
+                                      <div class="card card-block text-xs-left">
+                                      <h5  style={{color:"teal",marginTop:"10px"}}> <C style={{ color: "teal" }}/>Skills & Endorsement:</h5>
+            
+                                         <p style={{marginLeft:"20px",fontSize:"18px"}}>{badgeItems}</p>
+                                   </div>
+                                 </div>
+                                 </div> 
+                                 <div class="row" style={{marginTop:"15px"}}>
+                                 <div class="col-sm-12">
+                                      <div class="card card-block">
+                                      <h5 style={{color:"teal",marginTop:"10px"}}>  <C style={{ color: "teal" }}/> Achievements:</h5>
+            
+                                      {users.achievement.map((achievement,index)=>(
+                                          achievement!== null?<p style={{marginLeft:"20px",fontSize:"18px"}}>{achievement}</p>
+                                      :null
+                                      ))}
+                                   </div>
+                                 </div>
+                                 </div>     
+                            </div>
+                         </div>
+                     </div>               
+                 </div>
             </div>
+                  </div>
+                  </div>
+              </div>
         
             
             )});
     }
-    render(){
-    let config = {
-      num: [4, 7],
-      rps: 0.1,
-      radius: [5, 40],
-      life: [1.5, 3],
-      v: [2, 3],
-      tha: [-40, 40],
-      alpha: [0.6, 0],
-      scale: [.1, 0.4],
-      position: "all",
-      color: ["random", "#ff0000"],
-      cross: "dead",
-      // emitter: "follow",
-      random: 15
-    };
-
-    if (Math.random() > 0.85) {
-      config = Object.assign(config, {
-        onParticleUpdate: (ctx, particle) => {
-          ctx.beginPath();
-          ctx.rect(
-            particle.p.x,
-            particle.p.y,
-            particle.radius * 2,
-            particle.radius * 2
-          );
-          ctx.fillStyle = particle.color;
-          ctx.fill();
-          ctx.closePath();
-        }
-      });
-    } {
+    render() {
         return (
           <div>
           
-           <div class="bgrd">
-              <ParticlesBg  type="custom" config={config} bg={true} />
-          </div>
-            <div >
-            <div class="bgrd">
-              <ParticlesBg  type="custom" config={config} bg={true} />
-          </div>
+           
+            <div>
                 {this.fetch_users()}
             </div>
-            <div class="bgrd">
-              <ParticlesBg  type="custom" config={config} bg={true} />
-          </div>
+           
             </div>
         )
     }
     }
-}
+
 
 profilebyid.propTypes={
     auth:PropTypes.object.isRequired,
